c11b2339cedd923086fecf796619bf8c88c9b4ec
Murali Krishna K <88620472+kittenishkrish@users.noreply.github.com>
2023-10-27 18:00:37 +0530

    Add files via upload

--- Swing/src/swingExample/LoginPage.java
+++ Swing/src/swingExample/LoginPage.java
@@ -5,20 +5,22 @@ import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JButton;
 import javax.swing.border.Border;
 
 public class LoginPage {
 
 	public static void main(String[] args) {
 
-		Border border = BorderFactory.createLineBorder(Color.green,3);
+		Border border = BorderFactory.createLineBorder(Color.red,3);
 		
 		JFrame frame = new JFrame();
 		frame.setSize(500, 500);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setBackground(new Color(77, 184, 197));
+		frame.getContentPane().setBackground(new Color(4, 200, 249));
 		frame.setLayout(null);
 		frame.setTitle("Voyage");
+		frame.setResizable(false);
 
 		
 		JLabel label = new JLabel("Welcome To VR Voyage"); 
@@ -26,40 +28,46 @@ public class LoginPage {
 		label.setIcon(image);
 		label.setHorizontalTextPosition(JLabel.CENTER); 
 		label.setVerticalTextPosition(JLabel.TOP); 
-		label.setForeground(new Color(0x00FF00)); 
+		label.setForeground(new Color(6, 6, 6)); 
 		label.setFont(new Font("Roboto",Font.PLAIN,20)); 
 		label.setIconTextGap(20);
-		label.setBackground(Color.black); 
+		label.setBackground(new Color (6, 133, 161));
 		label.setOpaque(true); 
 		label.setBorder(border); 
 		label.setVerticalAlignment(JLabel.CENTER); 
 		label.setHorizontalAlignment(JLabel.CENTER); 
+		label.setBounds(60, 20, 50, 50); 
+		
+		JButton button =  new JButton("Submit");
+		Label username = new Label("Username");
+		TextField textarea = new TextField();
+		Label password = new Label("Password");
+		TextField textarea1 = new TextField();
+		button.setHorizontalTextPosition(JButton.CENTER);
+		button.setVerticalTextPosition(JButton.BOTTOM);
+		button.setForeground(Color.green);
+		button.setFocusable(false);
+		JButton button1 =  new JButton("Cancel");
+		//Button b = new Button("Submit");
+		
 		label.setBounds(100, 20, 250, 250); 
-		
-		
-		Label U = new Label("Username");
-		TextField t = new TextField();
-		Label p = new Label("Password");
-		TextField t1 = new TextField();
-		Button b = new Button("Submit");
-		
-		label.setBounds(100, 20, 250, 250); 
-		U.setBounds(20, 300, 80, 30);
-		t.setBounds(100, 300, 200, 30);
-		p.setBounds(20, 340, 80, 30);
-		t1.setBounds(100, 340, 200, 30);
-		b.setBounds(20, 380, 80, 30);
+		username.setBounds(20, 300, 80, 30);
+		textarea.setBounds(100, 300, 200, 30);
+		password.setBounds(20, 340, 80, 30);
+		textarea1.setBounds(100, 340, 200, 30);
+		button.setBounds(120, 400, 80, 30);
+		button1.setBounds(20, 400, 80, 30);
+		button1.setForeground(Color.red);
+		button1.setFocusable(false);
 		
 		frame.add(label);
-		frame.add(U);
-		frame.add(p);
-		frame.add(b);
-		frame.add(t1);
-		frame.add(t);
-
+		frame.add(username);
+		frame.add(password);
+		frame.add(button);
+		frame.add(textarea);
+		frame.add(textarea1);
+		frame.add(button1);
 		frame.setVisible(true);
-		
-			
+	
 	}
-
 }
